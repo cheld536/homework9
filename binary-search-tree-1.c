3242d7826e60eb77443a1984381b03e7239d5171
cheld536 <80086998+cheld536@users.noreply.github.com>
2021-05-04 19:55:28 +0900

    두번째 업로드
    두번째 업로드

--- binary-search-tree-1.c
+++ binary-search-tree-1.c
@@ -60,6 +60,7 @@ int main()
 			break;
 		case 'q': case 'Q':
 			freeBST(head);
+			printf("메모리 해제가 끝났습니다. 프로그램을 종료합니다. \n");
 			break;
 		case 'n': case 'N':
 			printf("Your Key = ");
@@ -125,46 +126,166 @@ int initializeBST(Node** h) {
 
 
 
-void inorderTraversal(Node* ptr)
+void inorderTraversal(Node* ptr)			// 중위순회
 {
-
+	if(ptr)
+	{
+		inorderTraversal(ptr->left);		// 왼쪽에 있는 노드 부터 먼저 들어간다.
+		printf("[%d] ",ptr->key);				// 중간에 있는 노드 부터 출력
+		inorderTraversal(ptr->right);		// 오른쪽에 있는 노드 탐색
+	}
 }
 
-void preorderTraversal(Node* ptr)
+void preorderTraversal(Node* ptr)			// 전위순회
 {
-
+	if(ptr)
+	{
+		printf("[%d] ",ptr->key);				// NULL이 아니라면 맨 처음 노드를 출력 부모노드 탐색후 자식 노드 탐색
+		preorderTraversal(ptr->left);		// 왼쪽부터 탐색
+		preorderTraversal(ptr->right);		// 오른쪽 탐색
+	}
 }
 
-void postorderTraversal(Node* ptr)
+void postorderTraversal(Node* ptr)			// 후위순회
 {
-
+	preorderTraversal(ptr->left);			// 왼쪽 탐색
+	preorderTraversal(ptr->right);			// 오른쪽 탐색
+	printf("[%d] ",ptr->key);					// 자식 노드 탐색 후 부모 노드 탐색
 }
 
 
 int insert(Node* head, int key)
 {
+	Node* new = (Node*)malloc(sizeof(Node)); // 사용자로부터 새로 입력 받을 노드를 동적할당해준다. 
+
+	new->key = key;							 // 노드를 입력받은 키값으로 초기화 해준다		
+	new->left = NULL;						 // 노드의 왼쪽 루트 가르키는 값을 널로 초기화
+	new->right = NULL;						 // 노드의 오른쪽 루트가 가르키는 값을 널로 초기화	
+	Node *findnode = head ->left;  			// 트리를 탐색할 노드를 선언하고 루트의 주소값을 저장한다.
+
+	if(head->left == NULL)					// 루트가 비었으면 new노드를 루트로 삽입한다.
+	{
+		head->left = new;
+	}	
+										// 만약 루트노드가 있다면 
+	while (findnode != NULL)			// NULL이 아닐때 까지 반복
+	{
+		if(findnode->key < key)				// 입력값이 더 클때
+		{
+			if(findnode->right == NULL)		// 노드의 오른쪽이 비었으면 
+			{
+				findnode->right = new;		// 오른쪽에 입력한 노드를 위치 시킨다.
+				break;						
+			}
+			findnode= findnode->right;		// 오른쪽이 차있으면 다음 노드로 넘어간다.
+		}
+		else								// 부모노드보다 작거나 같으면 
+		{
+			if(findnode->left == NULL)		// 왼쪽이 널이면 
+			{
+				findnode->left = new;		// 왼쪽에 입력한 노드를 위치 시킨다.
+				break;
+			}
+			findnode = findnode->left;		// 널이 아니면 왼쪽노드를 탐색한다.
+		}
+	}
+	
+
+	
 
 }
 
 int deleteLeafNode(Node* head, int key)
 {
+	Node* target = head->left;
+	Node* parents = NULL;
+	Node* remove = NULL;
+	while (target != NULL)
+	{
+		
+		if(key == target->key)
+			break;
+		if(key < target->key)
+		{
+			parents = target;
+			target = target->left;
+		}
+		else if ( key > target->key)
+		{
+			parents = target;
+			target = target->right;
+		}
+	}
+	
+	remove = target;
+	if(target==NULL)
+	{
+		printf("삭제하려는 %d 노드가 없습니다.\n",key);
+		return 0;
+	} 
+	if(target->left != NULL && target->right != NULL)
+	{
+		printf("삭제하려는 %d 노드는 리프노드가 아닙니다.\n");
+		return 0;
+	}
+
+	if(target->left == NULL && target->right == NULL)				//자식이 없는 단일 노드 일때
+	{
+		if(target->key == parents->left->key)
+		{
+			free(remove);
+			parents->left = NULL;
+		}
+		else if(target->key == parents->right->key)
+		{
+			free(remove);
+			parents->right = NULL;
+		}	
+	}
+	
+return 0;
 
 }
 
-Node* searchRecursive(Node* ptr, int key)
+Node* searchRecursive(Node* ptr, int key)           // 재귀함수로 이진탐색을 진행한다.
 {
-
+	if(!ptr) return NULL;							
+	if(key== ptr->key) return ptr;					// 찾고 있는 key값을 가진 노드의 주소를 반환
+	if(key<ptr->key)								// 입력된 key값보다 탐색된 key값이 더 크면
+		return searchRecursive(ptr->left,key);		// 왼쪽으로 이동한다.
+	return searchRecursive(ptr->right,key);			// 아니면 오른쪽으로 이동
 }
 
-Node* searchIterative(Node* head, int key)
+Node* searchIterative(Node* head, int key) 			// 반복적 탐색
 {
-
+	while(head)										// 헤드가 널이 아닐때까지 반복
+	{
+		if(key == head->key ) return head;			// key값을 찾으면 찾은 노드의 주소를 반환
+		if(key < head->key)							// 입력된 key값보다 탐색된 key값이 더 크면
+		{
+			head = head->left;						// 왼쪽으로 이동
+		}
+		else										// 아니면 오른쪽으로 이동 
+		{
+			head=head->right;
+		}
+	}
+	return NULL;
 }
 
 
 int freeBST(Node* head)
 {
-
+	if(head != NULL)					// 후위순회 탐색을 이용하여 자식 노드를 먼저 메모리 해제하고 부모노드를 해제한다.
+	{
+		freeBST(head->left);			
+		if (head->right != head)		// 루트 노드까지만 탐색한다.
+		{
+			freeBST(head->right);
+		}
+		free(head);						// 메모리를 해제 해준다.
+	}
+	
 }
 
 
