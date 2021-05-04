15aaf5e1da542c4338f955020cab722b37a8406b
cheld536 <80086998+cheld536@users.noreply.github.com>
2021-05-04 20:37:09 +0900

    3번째 업로드
    자료구조 9주차 과제 이진 탐색 트리 소스코드이다.

--- binary-search-tree-1.c
+++ binary-search-tree-1.c
@@ -13,8 +13,8 @@
 
 typedef struct node {
 	int key;
-	struct node *left;
-	struct node *right;
+	struct node* left;
+	struct node* right;
 } Node;
 
 int initializeBST(Node** h);
@@ -39,7 +39,7 @@ int main()
 	Node* head = NULL;
 	Node* ptr = NULL;	/* temp */
 
-	do{
+	do {
 		printf("\n\n");
 		printf("----------------------------------------------------------------\n");
 		printf("                   Binary Search Tree #1                        \n");
@@ -50,11 +50,13 @@ int main()
 		printf(" Preorder Traversal   = p      Search Node Iteratively      = f\n");
 		printf(" Postorder Traversal  = t      Quit                         = q\n");
 		printf("----------------------------------------------------------------\n");
+		printf("-----------------[ 김 윤 희 ]--------[ 2018038014 ]-------------\n");
+		printf("----------------------------------------------------------------\n");
 
 		printf("Command = ");
 		scanf(" %c", &command);
 
-		switch(command) {
+		switch (command) {
 		case 'z': case 'Z':
 			initializeBST(&head);
 			break;
@@ -76,7 +78,7 @@ int main()
 			printf("Your Key = ");
 			scanf("%d", &key);
 			ptr = searchIterative(head, key);
-			if(ptr != NULL)
+			if (ptr != NULL)
 				printf("\n node [%d] found at %p\n", ptr->key, ptr);
 			else
 				printf("\n Cannot find the node [%d]\n", key);
@@ -85,7 +87,7 @@ int main()
 			printf("Your Key = ");
 			scanf("%d", &key);
 			ptr = searchRecursive(head->left, key);
-			if(ptr != NULL)
+			if (ptr != NULL)
 				printf("\n node [%d] found at %p\n", ptr->key, ptr);
 			else
 				printf("\n Cannot find the node [%d]\n", key);
@@ -105,7 +107,7 @@ int main()
 			break;
 		}
 
-	}while(command != 'q' && command != 'Q');
+	} while (command != 'q' && command != 'Q');
 
 	return 1;
 }
@@ -113,7 +115,7 @@ int main()
 int initializeBST(Node** h) {
 
 	/* if the tree is not empty, then remove all allocated nodes from the tree*/
-	if(*h != NULL)
+	if (*h != NULL)
 		freeBST(*h);
 
 	/* create a head node */
@@ -128,19 +130,19 @@ int initializeBST(Node** h) {
 
 void inorderTraversal(Node* ptr)			// 중위순회
 {
-	if(ptr)
+	if (ptr)
 	{
 		inorderTraversal(ptr->left);		// 왼쪽에 있는 노드 부터 먼저 들어간다.
-		printf("[%d] ",ptr->key);				// 중간에 있는 노드 부터 출력
+		printf("[%d] ", ptr->key);				// 중간에 있는 노드 부터 출력
 		inorderTraversal(ptr->right);		// 오른쪽에 있는 노드 탐색
 	}
 }
 
 void preorderTraversal(Node* ptr)			// 전위순회
 {
-	if(ptr)
+	if (ptr)
 	{
-		printf("[%d] ",ptr->key);				// NULL이 아니라면 맨 처음 노드를 출력 부모노드 탐색후 자식 노드 탐색
+		printf("[%d] ", ptr->key);				// NULL이 아니라면 맨 처음 노드를 출력 부모노드 탐색후 자식 노드 탐색
 		preorderTraversal(ptr->left);		// 왼쪽부터 탐색
 		preorderTraversal(ptr->right);		// 오른쪽 탐색
 	}
@@ -150,49 +152,47 @@ void postorderTraversal(Node* ptr)			// 후위순회
 {
 	preorderTraversal(ptr->left);			// 왼쪽 탐색
 	preorderTraversal(ptr->right);			// 오른쪽 탐색
-	printf("[%d] ",ptr->key);					// 자식 노드 탐색 후 부모 노드 탐색
+	printf("[%d] ", ptr->key);					// 자식 노드 탐색 후 부모 노드 탐색
 }
 
 
 int insert(Node* head, int key)
 {
-	Node* new = (Node*)malloc(sizeof(Node)); // 사용자로부터 새로 입력 받을 노드를 동적할당해준다. 
+	Node* newnode = (Node*)malloc(sizeof(Node)); // 사용자로부터 새로 입력 받을 노드를 동적할당해준다. 
 
-	new->key = key;							 // 노드를 입력받은 키값으로 초기화 해준다		
-	new->left = NULL;						 // 노드의 왼쪽 루트 가르키는 값을 널로 초기화
-	new->right = NULL;						 // 노드의 오른쪽 루트가 가르키는 값을 널로 초기화	
-	Node *findnode = head ->left;  			// 트리를 탐색할 노드를 선언하고 루트의 주소값을 저장한다.
+	newnode->key = key;							 // 노드를 입력받은 키값으로 초기화 해준다		
+	newnode->left = NULL;						 // 노드의 왼쪽 루트 가르키는 값을 널로 초기화
+	newnode->right = NULL;						 // 노드의 오른쪽 루트가 가르키는 값을 널로 초기화	
+	Node* findnode = head->left;  			// 트리를 탐색할 노드를 선언하고 루트의 주소값을 저장한다.
 
-	if(head->left == NULL)					// 루트가 비었으면 new노드를 루트로 삽입한다.
+	if (head->left == NULL)					// 루트가 비었으면 new노드를 루트로 삽입한다.
 	{
-		head->left = new;
-	}	
-										// 만약 루트노드가 있다면 
+		head->left = newnode;
+	}
+	// 만약 루트노드가 있다면 
 	while (findnode != NULL)			// NULL이 아닐때 까지 반복
 	{
-		if(findnode->key < key)				// 입력값이 더 클때
+		if (findnode->key <= key)				// 입력값이 더 클때
 		{
-			if(findnode->right == NULL)		// 노드의 오른쪽이 비었으면 
+			if (findnode->right == NULL)		// 노드의 오른쪽이 비었으면 
 			{
-				findnode->right = new;		// 오른쪽에 입력한 노드를 위치 시킨다.
-				break;						
+				findnode->right = newnode;		// 오른쪽에 입력한 노드를 위치 시킨다.
+				break;
 			}
-			findnode= findnode->right;		// 오른쪽이 차있으면 다음 노드로 넘어간다.
+			findnode = findnode->right;		// 오른쪽이 차있으면 다음 노드로 넘어간다.
 		}
 		else								// 부모노드보다 작거나 같으면 
 		{
-			if(findnode->left == NULL)		// 왼쪽이 널이면 
+			if (findnode->left == NULL)		// 왼쪽이 널이면 
 			{
-				findnode->left = new;		// 왼쪽에 입력한 노드를 위치 시킨다.
+				findnode->left = newnode;		// 왼쪽에 입력한 노드를 위치 시킨다.
 				break;
 			}
 			findnode = findnode->left;		// 널이 아니면 왼쪽노드를 탐색한다.
 		}
 	}
-	
 
-	
-
+	return 0;
 }
 
 int deleteLeafNode(Node* head, int key)
@@ -200,74 +200,76 @@ int deleteLeafNode(Node* head, int key)
 	Node* target = head->left;
 	Node* parents = NULL;
 	Node* remove = NULL;
-	while (target != NULL)
+	while (target != NULL)								// 타겟이 널이 아닐때 까지 반복
 	{
-		
-		if(key == target->key)
+
+		if (key == target->key)							// 값을 찾았으면 반복문 중지
 			break;
-		if(key < target->key)
+		if (key < target->key)							// 입력된 값보다 찾는 값이 더 크면 
 		{
-			parents = target;
-			target = target->left;
+			parents = target;							// 부모노드에 탐색노드의 주소를 저장하고
+			target = target->left;						// 탐색노드는 왼쪽으로 이동
 		}
-		else if ( key > target->key)
+		else if (key > target->key)						// 입력된 값이 찾는 값보다 클때 
 		{
-			parents = target;
-			target = target->right;
+			parents = target;							// 부모노드에 탐색노드의 주소를 저장하고
+			target = target->right;						// 탐색노드는 오른쪽으로 이동
 		}
+
 	}
-	
-	remove = target;
-	if(target==NULL)
+
+	remove = target;									// 노드를 찾고 삭제할 노드의 주소를 저장
+	if (target == NULL)									// 노드가 없을 경우
 	{
-		printf("삭제하려는 %d 노드가 없습니다.\n",key);
+		printf("삭제하려는 %d 노드가 없습니다.\n", key);			
 		return 0;
-	} 
-	if(target->left != NULL && target->right != NULL)
+	}
+	if (target->left != NULL && target->right != NULL)			// 만약 자식노드가 존재할때
 	{
-		printf("삭제하려는 %d 노드는 리프노드가 아닙니다.\n");
+		printf("삭제하려는 %d 노드는 리프노드가 아닙니다.\n", key);
 		return 0;
 	}
 
-	if(target->left == NULL && target->right == NULL)				//자식이 없는 단일 노드 일때
+	if (target->left == NULL && target->right == NULL)				//자식이 없는 단일 노드 일때
 	{
-		if(target->key == parents->left->key)
+		if (target->key == parents->left->key)						// 찾는 값과 부모 노드의 왼쪽의 값이 같을때
 		{
-			free(remove);
-			parents->left = NULL;
+			free(remove);											// 자식 노드를 메모리 해제하고
+			parents->left = NULL;									// 왼쪽을 널값으로 초기화 한다.
 		}
-		else if(target->key == parents->right->key)
-		{
-			free(remove);
-			parents->right = NULL;
-		}	
+		else if (target->key == parents->right->key)				// 찾는 값과 부모 노드의 오른쪽 값이 같을때 
+		{	
+			free(remove);											// 자식노드를 메모리 해제하고
+			parents->right = NULL;									//오른쪽을 넓값으로 초기화 한다.
+		}
 	}
-	
-return 0;
+
+	return 0;
 
 }
 
 Node* searchRecursive(Node* ptr, int key)           // 재귀함수로 이진탐색을 진행한다.
 {
-	if(!ptr) return NULL;							
-	if(key== ptr->key) return ptr;					// 찾고 있는 key값을 가진 노드의 주소를 반환
-	if(key<ptr->key)								// 입력된 key값보다 탐색된 key값이 더 크면
-		return searchRecursive(ptr->left,key);		// 왼쪽으로 이동한다.
-	return searchRecursive(ptr->right,key);			// 아니면 오른쪽으로 이동
+	if (!ptr) return NULL;
+	if (key == ptr->key) return ptr;					// 찾고 있는 key값을 가진 노드의 주소를 반환
+	if (key < ptr->key)								// 입력된 key값보다 탐색된 key값이 더 크면
+		return searchRecursive(ptr->left, key);		// 왼쪽으로 이동한다.
+	return searchRecursive(ptr->right, key);			// 아니면 오른쪽으로 이동
 }
 
 Node* searchIterative(Node* head, int key) 			// 반복적 탐색
 {
-	while(head)										// 헤드가 널이 아닐때까지 반복
+	Node *target = head->left;
+	while (target)										// 헤드가 널이 아닐때까지 반복
 	{
-		if(key == head->key ) return head;			// key값을 찾으면 찾은 노드의 주소를 반환
-		if(key < head->key)							// 입력된 key값보다 탐색된 key값이 더 크면
+		if (key == target->key) return target;			// key값을 찾으면 찾은 노드의 주소를 반환
+		if (key < target->key)							// 입력된 key값보다 탐색된 key값이 더 크면
 		{
-			head = head->left;						// 왼쪽으로 이동
+			target = target->left;						// 왼쪽으로 이동
 		}
 		else										// 아니면 오른쪽으로 이동 
 		{
-			head=head->right;
+			target = target->right;
 		}
 	}
 	return NULL;
@@ -276,19 +278,15 @@ Node* searchIterative(Node* head, int key) 			// 반복적 탐색
 
 int freeBST(Node* head)
 {
-	if(head != NULL)					// 후위순회 탐색을 이용하여 자식 노드를 먼저 메모리 해제하고 부모노드를 해제한다.
+	if (head != NULL)					// 후위순회 탐색을 이용하여 자식 노드를 먼저 메모리 해제하고 부모노드를 해제한다.
 	{
-		freeBST(head->left);			
+		freeBST(head->left);
 		if (head->right != head)		// 루트 노드까지만 탐색한다.
 		{
 			freeBST(head->right);
 		}
 		free(head);						// 메모리를 해제 해준다.
 	}
-	
+	return 0;
 }
-
-
-
-
 
